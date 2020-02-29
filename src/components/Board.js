8be4036c4c321cfccf4840c59d86e344f9320caa
lokeshreddy <lokeshreddy160@gmail.com>
2020-02-29 12:09:33 +0530

    Add Components

---
+++ src/components/Board.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import List from "./List";
+
+class Board extends React.Component {
+  render() {
+    return (
+      <div>
+        <List />
+        <p>Borad</p>
+      </div>
+    );
+  }
+}
+
+export default Board;
